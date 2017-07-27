0f8213d3cc1930cef345b3b9cf486b4ad4fcb6b3
方正 <fangzheng428@163.com>
2017-07-27 09:06:17 +0800

    MineFragment add test 1

--- app/src/main/java/com/youdu/imooc_business/view/fragment/MineFragment.java
+++ app/src/main/java/com/youdu/imooc_business/view/fragment/MineFragment.java
@@ -1,9 +1,7 @@
 package com.youdu.imooc_business.view.fragment;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +12,7 @@ public class MineFragment extends Fragment {
 
     public MineFragment() {
         // Required empty public constructor
+      //test 1
     }
 
 
@@ -28,4 +27,6 @@ public class MineFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mine_layout, container, false);
     }
+
+
 }
