947733340b3cdc4c7b0fa9f211dfe26d1968e74c
fangzheng <185849480@qq.com>
2017-07-29 10:33:52 +0800

    add Test.java

---
+++ app/src/main/java/com/youdu/imooc_business/Test.java
@@ -0,0 +1,19 @@
+package com.youdu.imooc_business;
+
+import android.util.Log;
+
+/**
+ * <pre>
+ *     author : fangzheng
+ *     e-mail : 185849480@qq.com
+ *     time   : 2017/07/28
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class Test {
+    private static final String TAG = "Test";
+    public void show(){
+        Log.i(TAG, "show: ");
+    }
+}
